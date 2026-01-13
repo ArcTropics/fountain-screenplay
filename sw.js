097afe4590f273d3b585f48e8a02c4c4670aa8cc
Vinimay Kaul <vinimay.kaul@arctropics.com>
2026-01-13 18:48:14 +0700

    Fixes for PWA error

--- sw.js
+++ sw.js
@@ -1,26 +1,29 @@
 const CACHE_NAME = 'fountain-v1';
+// IMPORTANT: Every path must be exactly correct relative to sw.js
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './script.js',
-  './fountain.min.js'
+  './fountain.min.js',
+  './icon-512.png' // Make sure this file actually exists in your repo!
 ];
 
-// Install the service worker and cache files
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      // We use map to log which specific file fails
+      return Promise.all(
+        ASSETS.map(url => {
+          return cache.add(url).catch(err => console.error('Failed to cache:', url, err));
+        })
+      );
     })
   );
 });
 
-// Fetch files from cache if offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
